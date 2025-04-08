d24dab9fc788e1c4b1689198c3f9056a954c3dbb
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-04-08 13:05:33 +0200

    feat(ComputeCard): añadir la clase ComputeCard

---
+++ src/main/java/edu/estatuas/ComputeCard.java
@@ -0,0 +1,18 @@
+package edu.estatuas;
+
+public class ComputeCard {
+
+    private ScoreCard scoreCard;
+    GolfPlay golfPlay;
+
+    ComputeCard(ScoreCard scoreCard, GolfPlay golfPlay) {
+        this.scoreCard = scoreCard;
+        this.golfPlay = golfPlay;
+    }
+
+
+    public void compute(){
+
+
+    }
+}
