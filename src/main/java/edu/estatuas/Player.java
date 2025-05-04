1290efcc563c545eb9ab922157eca144835811b0
Mahu2121 <hugogbmahu@gmail.com>
2025-05-04 16:13:02 +0100

    feat (Player): crear setScore , getScore y el toString

--- src/main/java/edu/estatuas/Player.java
+++ src/main/java/edu/estatuas/Player.java
@@ -5,7 +5,7 @@ public class Player {
     private final String iniciales;
     private byte handicap = 0;
     private Short score = 0;
-    private Short Points
+    private Short points = 0;
 
     Player(String iniciales, byte handicap) {
         this.iniciales = iniciales;
@@ -20,14 +20,24 @@ public class Player {
         return handicap;
     }
 
-    public void setScore(int) {
-
+    public void setScore(int score) {
+        this.score = (short) score;
     }
 
     public Short getScore() {
-
+        return score;
     }
 
     public void setPoints(int points) {
+        this.points = (short) points;
+    }
+
+    public Short getPoints() {
+        return points;
+    }
+
+    @Override
+    public String toString() {
+        return getInitials() + " ---> H'cap: " + getHandicap() + "\t" + " Strokes: " + getScore() + "\t" + " Stableford points: " + getPoints();
     }
 }
